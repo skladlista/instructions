f332f1fd6f8353b83e1865f5d2e5877e592825ca
John Doe <johndoe@example.com>
2022-12-23 13:46:16 +0000

    Update 6 files

---
+++ src/OperatorSap/Components/Msc3n.jsx
@@ -0,0 +1,17 @@
+export default function Msc3n() {
+  return (
+    <div id="msc3n" className="article msc3n">
+      <p className="article__title">ПРОСМОТР ПАРТИИ</p>
+      <div className="article__div">
+        <ol>
+          <li className="article__subtitle">
+            <p>Транзакция MSC3N</p>
+            <ol className="article__list sublist">
+              <li>Для просмотра информации по партии в ERP</li>
+            </ol>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+}
